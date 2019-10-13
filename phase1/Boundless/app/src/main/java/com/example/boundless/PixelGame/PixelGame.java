018769e7b0f87f09a745da51bb26c79d753cb3fe
NyW8 <39530442+NyW8@users.noreply.github.com>
2019-10-13 18:12:25 -0400

    Initial template added for PixelGame.

---
+++ phase1/Boundless/app/src/main/java/com/example/boundless/PixelGame/PixelGame.java
@@ -0,0 +1,30 @@
+package com.example.boundless.PixelGame;
+
+import com.example.boundless.Game;
+
+import java.util.List;
+
+/**
+ * A game where you use pixels to recreate an image.
+ */
+public class PixelGame extends Game {
+    /**
+     * A list of all available levels.
+     */
+    List<int[][]> levels;
+
+    /**
+     * The user's choices of pixels.
+     */
+    int[][] userChoice;
+
+    /**
+     * Checks if the users choices are correct.
+     *
+     * @return A boolean telling if the answer is correct.
+     */
+    public boolean checkPixels() {
+        //TODO
+        return false;
+    }
+}
