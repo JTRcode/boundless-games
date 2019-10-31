a3bedd957b91663fe3ef85c7f129dae81e53b266
hanjia6 <jackson@mail.utoronto.ca>
2019-10-30 21:05:22 -0400

    added start and end pipes. bug fix, hitting back will cause error while in Game

--- phase1/Boundless/app/src/main/java/com/example/boundless/RotateTileGame/RotateTileGame.java
+++ phase1/Boundless/app/src/main/java/com/example/boundless/RotateTileGame/RotateTileGame.java
@@ -15,7 +15,7 @@ public class RotateTileGame extends Game {
     /**
      * The manager for this game
      */
-    private TileManager manager = new TileManager(5);
+    private TileManager manager = new TileManager(4);
     /**
      * The users choices of the tiles
      */
@@ -25,8 +25,11 @@ public class RotateTileGame extends Game {
      */
     private Paint paint;
 
+
     public RotateTileGame(Context context) {
         super(context);
+        Tile startPipe = manager.createNewTile(TileEnum.I);
+        startPipe.setTile(Rotation.EAST);
         userChoice = manager.getTileStage();
         paint = new Paint();
         paint.setColor(Color.WHITE);
@@ -36,18 +39,25 @@ public class RotateTileGame extends Game {
     @Override
     public boolean gameOver() {
         //TODO don't use a try catch for phase 2
-        if (manager.gameOver(userChoice)){
-            try {userChoice = manager.getTileStage();}
-            catch(IndexOutOfBoundsException e){
+        if (manager.gameOver(userChoice)) {
+            try {
+                userChoice = manager.getTileStage();
+            } catch (IndexOutOfBoundsException e) {
                 //out of levels, throws an exception
                 return true;
             }
         }
+        Tile startPipe = manager.createNewTile(TileEnum.I);
+        startPipe.setTile(Rotation.EAST);
         return false;
     }
 
     @Override
     public void draw(Canvas canvas) {
+        Tile startPipe = manager.createNewTile(TileEnum.I);
+        startPipe.setTile(Rotation.EAST);
+        canvas.drawBitmap(startPipe.rotatedImage, manager.getStartX() - manager.getTileSize(), manager.getStartY(), paint);
+        canvas.drawBitmap(startPipe.rotatedImage, manager.getStartX() + (manager.getGridSize()) * manager.getTileSize(), manager.getStartY() + (manager.getGridSize() - 1) * manager.getTileSize(), paint);
         for (int i = 0; i < userChoice.length; i++) {
             for (int j = 0; j < userChoice.length; j++) {
                 canvas.drawBitmap(userChoice[i][j].rotatedImage,
@@ -65,8 +75,8 @@ public class RotateTileGame extends Game {
      */
     @Override
     public void screenTouched(int x, int y) {
-        int i = (y - manager.getStartY())/manager.getTileSize();
-        int j = (x - manager.getStartX())/manager.getTileSize();
+        int i = (y - manager.getStartY()) / manager.getTileSize();
+        int j = (x - manager.getStartX()) / manager.getTileSize();
         if (i < manager.getGridSize() && j < manager.getGridSize())
             userChoice[i][j].rotateTile();
     }
